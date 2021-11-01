fc54b0d8eade3a4940594648de0cc37438bda87d
Mahdiba97 <mahdi.bahmanpour97@gmail.com>
2021-11-02 01:24:39 +0330

    Implement Dagger

---
+++ app/src/main/java/com/mahdiba97/board/BoardApp.kt
@@ -0,0 +1,7 @@
+package com.mahdiba97.board
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class BoardApp : Application()
--- app/src/main/java/com/mahdiba97/board/feature_note/data/data_source/NoteDatabase.kt
+++ app/src/main/java/com/mahdiba97/board/feature_note/data/data_source/NoteDatabase.kt
@@ -7,4 +7,7 @@ import com.mahdiba97.board.feature_note.domine.model.Note
 @Database(entities = [Note::class], version = 1)
 abstract class NoteDatabase : RoomDatabase() {
     abstract val noteDao: NoteDao
+    companion object{
+        const val DATABASE_NAME = "notes_db"
+    }
 }
