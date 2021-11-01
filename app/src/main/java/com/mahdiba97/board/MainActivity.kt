b4d409ed95ae811c8f0cee32623d8528ef6d3523
Mahdiba97 <mahdi.bahmanpour97@gmail.com>
2021-11-01 22:06:07 +0330

    Initial commit

---
+++ app/src/main/java/com/mahdiba97/board/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.mahdiba97.board
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.mahdiba97.board.ui.theme.BoardTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BoardTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    BoardTheme {
+        Greeting("Android")
+    }
+}
